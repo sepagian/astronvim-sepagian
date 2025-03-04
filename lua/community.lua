dde3a02d240dc76b8a218f8398dcc35b64d38853
sepagian <rizkydana14@gmail.com>
2025-03-04 08:53:58 +0800

    Update AI recipes

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
