b883ca12d309f74ae09335f0a836c43487a5431f
sepagian <rizkydana14@gmail.com>
2025-03-04 09:05:54 +0800

    Modified config for completion integration

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,10 +1,33 @@
+---@type LazySpec
+---
 return {
-  {
-    "Exafunction/codeium.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  event = "InsertEnter",
+  build = ":Codeium Auth",
+  opts = {
+    virtual_text = {
+      key_bindings = {
+        accept = false, -- handled by completion engine
+      },
     },
-    config = function() require("codeium").setup() end,
+  },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        options = {
+          g = {
+            -- set the ai_accept function
+            ai_accept = function()
+              if require("codeium.virtual_text").get_current_completion_item() then
+                vim.api.nvim_input(require("codeium.virtual_text").accept())
+                return true
+              end
+            end,
+          },
+        },
+      },
+    },
   },
 }
