0534f1447aba4586a2a6740a0cb6a4c9b4674982
sepagian <rizkydana14@gmail.com>
2025-03-04 09:09:07 +0800

    Removed AI completion recipes

--- lua/community.lua
+++ lua/community.lua
@@ -6,7 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
