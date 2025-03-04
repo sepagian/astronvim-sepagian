2469a93e0eed81aed3acd27e201fc245d9b013d4
sepagian <rizkydana14@gmail.com>
2025-03-04 11:07:13 +0800

    Modified community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -7,7 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
